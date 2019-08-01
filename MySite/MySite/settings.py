a53a0ea3069ab5f9889f54785db302a46bff2131
devilangelwing <yuvan.sb21@gmail.com>
2019-08-01 23:43:24 +0530

    test

--- MySite/MySite/settings.py
+++ MySite/MySite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'MySite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'igproject',
+        'USER': 'root',
+        'PASSWORD': 'infy@123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
