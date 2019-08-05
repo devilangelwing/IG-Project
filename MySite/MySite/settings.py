d1e5323fc4c2664e9e55d173304dc0d55d7a043b
devilangelwing <yuvan.sb21@gmail.com>
2019-08-05 22:52:37 +0530

    login page

--- MySite/MySite/settings.py
+++ MySite/MySite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap',
+    'fontawesome',
     'login.apps.LoginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ( os.path.join('static'), )
